86258fd0e220afad989a68edcc9146860d6384ce
dharmenderchoudhary99 <dharmenderchoudhary022@gmail.com>
2024-04-25 10:49:46 +0530

    Adding New Recipe through Submit Form and Deleting

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -144,94 +144,82 @@ exports.exploreRandom = async (req, res) => {
  *Submit Recipe
  */
 exports.submitRecipe = async (req, res) => {
+  const infoErrorObj = req.flash("infoErrors");
+  const infoSubmitObj = req.flash("infoSubmit");
+
   res.render("submit-recipe", {
     title: "Cooking Blog - Submit Recipe",
+    infoErrorObj,
+    infoSubmitObj,
   });
 };
-
 
 /*
  *POST /submit-recipe
  *Submit Recipe
  */
- exports.submitRecipeOnPost = async (req, res) => {
-  res.redirect("submit-recipe");
+exports.submitRecipeOnPost = async (req, res) => {
+  try {
+    let imageUploadFile;
+    let uploadPath;
+    let newImageName;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+      console.log("No files where uploaded");
+    } else {
+      imageUploadFile = req.files.image;
+      newImageName = Date.now() + imageUploadFile.name;
+
+      uploadPath =
+        require("path").resolve("./") + "/public/uploads/" + newImageName;
+      imageUploadFile.mv(uploadPath, function (err) {
+        if (err) return res.status(500).send(err);
+      });
+    }
+
+    const newRecipe = new Recipe({
+      name: req.body.name,
+      description: req.body.description,
+      email: req.body.email,
+      ingredients: req.body.ingredients,
+      category: req.body.category,
+      image: newImageName,
+    });
+
+    await newRecipe.save();
+
+    req.flash("infoSubmit", "Recipe has been submitted");
+    res.redirect("/submit-recipe");
+  } catch (error) {
+    res.json(error);
+    req.flash("infoSubmit", error);
+    res.redirect("/submit-recipe");
+  }
 };
 
+/*Update  Recipe */
 
-
-
-
-
-
-// async function insertDummyCategoryData() {
+// async function updateRecipe() {
 //   try {
-//     await Category.insertMany([
-//       {
-//         name: "Thai",
-//         image: "thai-food.jpg",
-//       },
-//       {
-//         name: "American",
-//         image: "american-food.jpg",
-//       },
-//       {
-//         name: "Chinese",
-//         image: "chinese-food.jpg",
-//       },
-//       {
-//         name: "Mexican",
-//         image: "mexican-food.jpg",
-//       },
-//       {
-//         name: "Indian",
-//         image: "indian-food.jpg",
-//       },
-//       {
-//         name: "Spanish",
-//         image: "spanish-food.jpg",
-//       },
-//     ]);
-//   } catch (error) {
-//     console.log("error ", +error.message);
-//   }
-// }
-
-// insertDummyCategoryData();
-
-// async function insertDummyRecipeData() {
-//   try {
-//     await Recipe.insertMany(
-//       [
-//             {
-//                "name": "Recipe Name Goes Here",
-//               "description": `Recipe Description Goes Here`,
-//                "email": "recipeemail@raddy.co.uk",
-//               "ingredients": [
-//                  "1 level teaspoon baking powder",
-//                   "1 level teaspoon cayenne pepper",
-//                   "1 level teaspoon hot smoked paprika",
-//                 ],
-//                "category": "American",
-//                 "image": "southern-friend-chicken.jpg"
-//              },
-//              {
-//                 "name": "Recipe Name Goes Here",
-//                 "description": `Recipe Description Goes Here`,
-//                "email": "recipeemail@raddy.co.uk",
-//                "ingredients": [
-//                    "1 level teaspoon baking powder",
-//                  "1 level teaspoon cayenne pepper",
-//                   "1 level teaspoon hot smoked paprika",
-//                ],
-//                "category": "American",
-//                  "image": "southern-friend-chicken.jpg"
-//               },
-//              ]
+//     const res = await Recipe.updateOne(
+//       { name: "New Recipe" },
+//       { name: "New Recipe Updated" }
 //     );
+//     res.m;
+//     res.nModified;
 //   } catch (error) {
-//     console.log("error ", +error.message);
+//     console.log(error);
 //   }
 // }
+// updateRecipe();
 
-// insertDummyRecipeData();
+/*Delete Recipe */
+
+// async function deleteRecipe() {
+//   try {
+//     await Recipe.deleteOne({ name: "New Recipe Updated" });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// deleteRecipe();
